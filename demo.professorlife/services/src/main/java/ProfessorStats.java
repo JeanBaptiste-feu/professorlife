4014069b5f0d68db2c1c3b3164aaec7c0c5fc349
PETIT Jean-Baptiste <jeanbpet@gmail.com>
2021-08-21 17:32:33 +0200

    init

---
+++ demo.professorlife/services/src/main/java/ProfessorStats.java
@@ -0,0 +1,18 @@
+public class ProfessorStats {
+
+    String name;
+    int age;
+
+    /**
+     * Player embodies a professor managing student
+     * This class describe professor statistics
+     * @param nameParam
+     *      name of player's professor
+     * @param ageParam
+     *      age of player's professor
+     */
+    public ProfessorStats(String nameParam, int ageParam){
+        this.name = nameParam;
+        this.age = ageParam;
+    }
+}
---
+++ demo.professorlife/src/main/java/com/example/feudhivert/demo/professorlife/Application.java
@@ -0,0 +1,13 @@
+package com.example.feudhivert.demo.professorlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		//SpringApplication.run(Application.class, args);
+	}
+}
